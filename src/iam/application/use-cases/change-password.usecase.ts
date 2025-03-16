9717c901991fbc1f0fc604bf8d5d2ce822b543ac
indunil-jay <indunil.jayz@gmail.com>
2025-03-16 20:40:02 +0530

    refactor : refactor the change-password usecase

--- src/iam/application/use-cases/change-password.usecase.ts
+++ src/iam/application/use-cases/change-password.usecase.ts
@@ -2,25 +2,48 @@ import { getInjection } from "@/di/container";
 import { changePasswordDto } from "@/src/iam/application/dto/change-password.dto";
 import { User } from "@/src/iam/domain/user.entity";
 import { IncorrectPasswordException } from "@/src/iam/application/exceptions/specific.exceptions";
-import { PasswordChangedEvent } from "../../domain/events/password-changed.event";
+import { PasswordChangedEvent } from "@/src/iam/domain/events/password-changed.event";
+import { IHashingService } from "@/src/iam/application/services/hashing.service";
+import { IUserRepository } from "@/src/iam/application/repositories/user.repository";
 
 export const changePasswordUseCase = {
     async execute({ newPassword, currentPassword }: changePasswordDto) {
-        const authenticationService = getInjection("IAuthenticationService");
-        const eventBus = getInjection("IEventBus");
+        const {
+            authenticationService,
+            eventBus,
+            hashingService,
+            userRepository,
+        } = this.getServices();
         //check session
         const user = await authenticationService.verifySessionUser();
         //check if password is correct
-        await this.verifyPassword(currentPassword, user);
+        await this.verifyPassword(currentPassword, user, hashingService);
         //if,so create new password
-        await this.updateNewPassword(newPassword, user);
+        await this.updateNewPassword(
+            newPassword,
+            user,
+            userRepository,
+            hashingService
+        );
 
         //public password changed event
         await eventBus.publish(new PasswordChangedEvent(user));
     },
 
-    async verifyPassword(currentPassword: string, user: User) {
-        const hashingService = getInjection("IHashingService");
+    getServices() {
+        return {
+            authenticationService: getInjection("IAuthenticationService"),
+            eventBus: getInjection("IEventBus"),
+            hashingService: getInjection("IHashingService"),
+            userRepository: getInjection("IUserRepository"),
+        };
+    },
+
+    async verifyPassword(
+        currentPassword: string,
+        user: User,
+        hashingService: IHashingService
+    ) {
         const isCorrect = await hashingService.compare(
             currentPassword,
             user.password!
@@ -30,9 +53,12 @@ export const changePasswordUseCase = {
         }
     },
 
-    async updateNewPassword(newPassword: string, user: User) {
-        const hashingService = getInjection("IHashingService");
-        const userRepository = getInjection("IUserRepository");
+    async updateNewPassword(
+        newPassword: string,
+        user: User,
+        userRepository: IUserRepository,
+        hashingService: IHashingService
+    ) {
         const hashedPassword = await hashingService.hash(newPassword);
 
         await userRepository.update(user.id, {
